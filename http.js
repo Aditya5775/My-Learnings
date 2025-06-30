bd8b428a63e4bb70c189b2fd65620e08fa69bfc6
ADITYA R <104893678+Aditya5775@users.noreply.github.com>
2025-06-30 16:00:25 +0530

    Add files via upload

---
+++ http.js
@@ -0,0 +1,10 @@
+ console.log('Hello World');
+
+ //modules
+ const add= require('./server')
+ console.log(server.sum(1,2));
+ console.log(server.PI);
+ console.log(new server.math());
+
+ 
+
---
+++ server.js
@@ -0,0 +1,9 @@
+//modules
+const sum = (num1,num2) => num1 + num2;
+const PI = 3.14;
+class math {
+  constructor() {
+    console.log('object created')
+  }
+}
+module.exports ={sum : sum, PI : PI, math : math};
